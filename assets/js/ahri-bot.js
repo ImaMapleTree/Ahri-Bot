550a178a4adc4b41f74d6562b07b28fcfbcf4ccc
BuildTools <unconfigured@null.spigotmc.org>
2020-02-02 04:42:30 -0500

    Fixing previous issues

--- assets/js/ahri-bot.js
+++ assets/js/ahri-bot.js
@@ -1,12 +1,9 @@
-import 'https://unpkg.com/react@16/umd/react.development.js';
-
 $(document).ready(function () {
 	$.getJSON("https://api.jsonbin.io/b/5e1ea24db236b871b3621d94/latest", function (json) {
 		var bad_solution = document.getElementById("proudly-used-by-loading-servers");
 		var server_container = bad_solution.childNodes[1];
 		server_container.setAttribute("style", "color: #ff9b0f;");
 		server_container.innerHTML = json.servers;
-		console.log(process.env.REACT_APP_API_KEY);
 	})
 });
 
