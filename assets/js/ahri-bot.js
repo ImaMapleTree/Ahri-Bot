79098429ad7842154cd3eaeeb137713572f5b68e
BuildTools <unconfigured@null.spigotmc.org>
2020-02-02 03:36:23 -0500

    Implementation of the highlight feature on the website

--- assets/js/ahri-bot.js
+++ assets/js/ahri-bot.js
@@ -1,9 +1,13 @@
+import './App.css';
+
+
 $(document).ready(function () {
 	$.getJSON("https://api.jsonbin.io/b/5e1ea24db236b871b3621d94/latest", function (json) {
 		var bad_solution = document.getElementById("proudly-used-by-loading-servers");
 		var server_container = bad_solution.childNodes[1]
 		server_container.setAttribute("style", "color: #ff9b0f;");
 		server_container.innerHTML = json.servers
+		console.log(process.env.REACT_APP_API_KEY)
 	})
 });
 
