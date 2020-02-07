959e5405ebbeed5ec33686635ec3da0b3de167f6
BuildTools <unconfigured@null.spigotmc.org>
2020-02-06 19:41:09 -0500

    Background work for the upload/highlight feature

--- assets/js/ahri-bot.js
+++ assets/js/ahri-bot.js
@@ -1,12 +1,3 @@
-$(document).ready(function () {
-	$.getJSON("https://api.jsonbin.io/b/5e1ea24db236b871b3621d94/latest", function (json) {
-		var bad_solution = document.getElementById("proudly-used-by-loading-servers");
-		var server_container = bad_solution.childNodes[1];
-		server_container.setAttribute("style", "color: #ff9b0f;");
-		server_container.innerHTML = json.servers;
-	})
-});
-
 function GoPage(page, tab=false) {
 	if (tab == true) {
 		window.open(page);
@@ -16,8 +7,8 @@ function GoPage(page, tab=false) {
 	}
 }
 
-function ECCCheck() {
-	var ECC = "ECC=";
+function CookieCheck(cname) {
+	var name = cname + "=";
 	var decodedCookie = decodeURIComponent(document.cookie);
 	var ca = decodedCookie.split(';');
 	for(var i = 0; i <ca.length; i++) {
@@ -29,4 +20,77 @@ function ECCCheck() {
       return c.substring(name.length, c.length);
     }
   }
+}
+
+function CSFT(str, amount) {
+	if (amount < 0)
+		return CSFT(str, amount + 26);
+
+	var output = '';
+
+	for (var i = 0; i < str.length; i ++) {
+
+		var c = str[i];
+
+		if (c.match(/[a-z]/i)) {
+
+			var code = str.charCodeAt(i);
+
+			if ((code >= 65) && (code <= 90))
+				c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+			
+			else if ((code >= 97) && (code <= 122))
+				c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+
+		}
+		output += c;
+
+	}
+	return output;
+};
+
+function CheckAhri() {
+	
+	var exampleSocket = new WebSocket("wss://107.217.234.59:25565", ["protocolOne", "protocolTwo"]);
+}
+
+
+function DEC(CL) {
+	var alphabet = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ").split("");
+	var SLength = CL.length;
+	var FSplit = CL.slice(CL.length-2);
+	var L = FSplit[0];
+	var LL = FSplit[1];
+	var L1 = alphabet.indexOf(L);
+	var L2 = alphabet.indexOf(LL);
+	if (L2 >= L1) {
+		var TM = (L2 - L1) * -1;
+	} else {
+		var TM = (L1 - L2) * -1;
+	}
+	var DDS = CSFT(CL, TM);
+	var TIOBV = DDS.indexOf("IWNPC");
+	var DS2 = DDS.slice(TIOBV+5);
+	var FDS = CSFT(DS2, TM*-1);
+	var MCTBIWTD = FDS.slice(FDS.length-2);
+	var MK = FDS.replace(MCTBIWTD, "");
+	var MTY = MK.split("D");
+	var OPT = MTY[0]%MTY[1];
+	var pos1 = 0;
+	var pos2 = 1;
+	var ALLCHAR = DDS.replace(DS2, "")
+	var ALLCHAR2 = ALLCHAR.split("")
+	var NS = ''
+	for (i = 0; i < ALLCHAR2.length; i++) {
+		var INTYON = parseInt(ALLCHAR2[i]) - OPT;
+		if (INTYON <= -1) {
+			INTYON = INTYON + 10;
+		}
+		if (isNaN(ALLCHAR2[i]) == true) {
+			INTYON = ALLCHAR2[i];
+		}
+		var NS = NS + INTYON
+	}
+	NS = NS + FDS
+	console.log(NS)	
 }
